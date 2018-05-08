0e9d6a26d191241b9ede24c705d121d4d1ee676e
uanoob <usnet@ukr.net>
2018-05-08 15:11:18 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
